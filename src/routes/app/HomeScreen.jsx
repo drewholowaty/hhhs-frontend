253ccf1822d058e4513f1e58d5c16418ab1dbef2
Drew Holowaty <drewholowaty@gmail.com>
2022-06-06 22:43:41 -0400

    broken attempt at fetching data for home page

--- src/routes/app/HomeScreen.jsx
+++ src/routes/app/HomeScreen.jsx
@@ -1,6 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
 function HomeScreen() {
+    const [data, setData] = useState({ hits: [] });
+    useEffect(() => {
+        async function fetchData() {
+            // You can await here
+            const response = await fetch('https://yb67zyc3bj.execute-api.us-east-2.amazonaws.com/prod');
+            setData(response.data);
+        }
+        fetchData();
+    
+      }, []);
+    console.log(data);
     return (
         <h1>hello home</h1>
+
     );
 }
   
